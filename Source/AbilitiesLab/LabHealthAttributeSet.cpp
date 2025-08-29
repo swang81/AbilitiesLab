5c08adfeefc0b150d6d91af51d8de1f0770ca5b8
wangshuai <swang81@icloud.com>
2025-08-29 21:00:02 +0800

    temp

--- Source/AbilitiesLab/LabHealthAttributeSet.cpp
+++ Source/AbilitiesLab/LabHealthAttributeSet.cpp
@@ -13,6 +13,53 @@ void ULabHealthAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimePr
 	DOREPLIFETIME(ULabHealthAttributeSet, MaxHealth);
 }
 
+void ULabHealthAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT("PreChange: Attribute '%s'"), *Attribute.AttributeName);
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+
+	Super::PreAttributeChange(Attribute, NewValue);
+}
+
+
+
+void ULabHealthAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+	UE_LOG(LogTemp,Warning, TEXT("Post Change: Attribute '%s' changed %.2f -> %.2f"),*Attribute.AttributeName, OldValue, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		OnHealthChanged.Broadcast(this, OldValue, NewValue);
+	}
+	else if (Attribute == GetMaxHealthAttribute())
+	{
+		const float CurrentHealth = GetHealth();
+		OnHealthChanged.Broadcast(this, CurrentHealth, CurrentHealth);
+	}
+	
+}
+
+void ULabHealthAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ULabHealthAttributeSet, Health, OldValue);
+	const float OldHealth = OldValue.GetCurrentValue();
+	const float NewHealth = GetHealth();
+	OnHealthChanged.Broadcast(this, OldHealth, NewHealth);
+	
+}
+
+void ULabHealthAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ULabHealthAttributeSet, MaxHealth, OldValue);
+	const float CurrentHealth = GetHealth();
+	OnHealthChanged.Broadcast(this, CurrentHealth, CurrentHealth);
+	
+}
+
 ULabHealthAttributeSet::ULabHealthAttributeSet()
 {
 	InitHealth(100.0f);
--- Source/AbilitiesLab/LabHealthAttributeSet.h
+++ Source/AbilitiesLab/LabHealthAttributeSet.h
@@ -11,11 +11,15 @@
  * 
  */
 
+
 #define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
 GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FAttributeChangedEvent, UAttributeSet*, AttributeSet, float, OldValue, float, NewValue);
 
 UCLASS()
 class ABILITIESLAB_API ULabHealthAttributeSet : public UAttributeSet
@@ -26,14 +30,31 @@ public:
 	ULabHealthAttributeSet();
 	
 	void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
+	
+
+	UPROPERTY(BlueprintAssignable)
+	FAttributeChangedEvent OnHealthChanged;
+
+	
 	// Current health
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, ReplicatedUsing = OnRep_Health)
 	FGameplayAttributeData Health;
 	
 	// Upper limit for health value
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Replicated)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth)
 	FGameplayAttributeData MaxHealth;
 	
 	ATTRIBUTE_ACCESSORS(ULabHealthAttributeSet, Health);
 	ATTRIBUTE_ACCESSORS(ULabHealthAttributeSet, MaxHealth);
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+
 };
